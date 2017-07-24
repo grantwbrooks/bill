b041dab8c31ad7d7d32fa4a97e4a0aa17dcf1408
grantwbrooks <grantwbrooks@gmail.com>
2017-07-24 17:03:22 -0400

    finished bill assignment

---
+++ main.js
@@ -0,0 +1,6 @@
+            var reward = .01;
+            for(var i = 1; i <=30; i++) {
+                reward *= 2;
+                console.log(reward);
+            }
+            console.log(reward);
